2266d7d5130d1ca4293a24064129734cf513b4b6
0dminnimda <0dminnimda@gmail.com>
2025-07-07 20:27:18 +0300

    Add types.h

---
+++ source/types.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdint.h>
+#include <inttypes.h>
+
+/* Signed numbers */
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+/* Unsigned numbers */
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+/* Floating point numbers */
+typedef float f32;
+typedef double f64;
+typedef long double f80;
+
+#endif  // TYPES_H
+
