fba710e2caa29c2ad42c12458963be9cc539c769
0dminnimda <0dminnimda@gmail.com>
2025-07-08 23:13:44 +0300

    Add print.hpp and delete hprint.hpp

--- source/main.cpp
+++ source/main.cpp
@@ -3,36 +3,42 @@
 #include "types.h"
 
 #include <stdio.h>
+#define STR_IMPLEMENT
 #include "array.hpp"
+#include "print.hpp"
 
 int main() {
     Array<int> my_ints = {};
 
-    printf("Pushing elements...\n");
-    array_push(&my_ints, 10);
-    array_push(&my_ints, 20);
-    array_push(&my_ints, 30);
+    print("adding elements...\n");
+    array_add(&my_ints, 10);
+    array_add(&my_ints, 20);
+    array_add(&my_ints, 30);
 
-    printf("Array has %zu elements, capacity for %zu\n", my_ints.length, my_ints.capacity);
-    printf("Element at index 1: %d\n", my_ints[1]);
+    print("Array has % elements, capacity for %\n", my_ints.length, my_ints.capacity);
+    print("Element at index 1: %\n", my_ints[1]);
 
-    printf("Iterating with range-based for loop:\n");
+    int new_data[] = {4, 5, 6, 7, 8, 9, 10};
+    array_add_range(&my_ints, new_data, 7);
+    print("Array has % elements, capacity for %\n", my_ints.length, my_ints.capacity);
+
+    print("Iterating with range-based for loop:\n");
     for (int val : my_ints) {
         printf("  - %d\n", val);
     }
 
     int popped_value = array_pop(&my_ints);
-    printf("Popped value: %d\n", popped_value);
-    printf("Array now has %zu elements\n", my_ints.length);
+    print("Popped value: %\n", popped_value);
+    print("Array now has % elements %\n", my_ints.length, 1);
 
     array_free(&my_ints);
-    printf("Array freed. Length is now %zu\n", my_ints.length);
+    print("Array freed. Length is now %-%\n", my_ints.length, str("hello"));
 
-    printf("\nCreating an array with a macro:\n");
-    Array<float> my_floats = array_of(1.1f, 2.2f, 3.3f, 4.4f);
+    print("\nCreating an array with a macro:\n");
+    Array<float> my_floats = array_of(1.1f, 2.2f, 3.3456f, 4.4f);
 
     for (float f : my_floats) {
-        printf("  - %f\n", f);
+        print("  - %\n", f);
     }
 
     array_free(&my_floats);
@@ -60,15 +66,15 @@ int main() {
 /*    auto a_r = str_to_int(a, &a_i);*/
 /*    auto b_r = str_to_int(b, &b_i);*/
 /*    auto c_r = str_to_int(c, &c_i);*/
-/*    /*auto d_r = str_to_int(d, &d_i);* /*/
-/*    /*auto e_r = str_to_int(e, &e_i);* /*/
-/*    /*auto f_r = str_to_int(f, &f_i);* /*/
+/*    / *auto d_r = str_to_int(d, &d_i);* /*/
+/*    / *auto e_r = str_to_int(e, &e_i);* /*/
+/*    / *auto f_r = str_to_int(f, &f_i);* /*/
 /**/
 /*    print("% -> % (%)\n", a, a_i, (u8)a_r);*/
 /*    print("% -> % (%)\n", b, b_i, (u8)b_r);*/
 /*    print("% -> % (%)\n", c, c_i, (u8)c_r);*/
-/*    /*print("% -> % (%)\n", d, d_i, (u8)d_r);* /*/
-/*    /*print("% -> % (%)\n", e, e_i, (u8)e_r);* /*/
-/*    /*print("% -> % (%)\n", f, f_i, (u8)f_r);* /*/
+/*    / *print("% -> % (%)\n", d, d_i, (u8)d_r);* /*/
+/*    / *print("% -> % (%)\n", e, e_i, (u8)e_r);* /*/
+/*    / *print("% -> % (%)\n", f, f_i, (u8)f_r);* /*/
 /*    return 0;*/
 /*}*/
---
+++ source/print.hpp
@@ -0,0 +1,131 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+
+#include <type_traits>  // For std::is_... traits
+#include <charconv>     // For std::to_chars
+#include <system_error> // For std::errc
+
+#include "array.hpp" // string builder
+#include "str.hpp" // string builder
+
+
+namespace print_detail {
+
+template<typename T>
+void h_format_arg(Array<char>* builder, const T& value);
+
+void print_impl_recursive(Array<char>* builder, const char* fmt);
+
+template<typename T, typename... Rest>
+void print_impl_recursive(Array<char>* builder, const char* fmt, T&& arg, Rest&&... rest);
+
+} // namespace print_detail
+
+
+/**
+ * Type-safe, extensible string formatting that returns a new `str`.
+ *
+ * This function provides type-safe formatting similar to C++20 std::format or Python f-strings.
+ * - It does NOT use `std::string` at any point.
+ * - It returns a new, heap-allocated `str` object. The caller is responsible for
+ *   freeing this `str` using `str_free`.
+ * - To extend it for your own type `MyType`, provide the following function in
+ *   the same namespace as your type (or in the global namespace):
+ *   `void to_chars_custom(Array<char>& builder, const MyType& val)`
+ */
+template<typename... Args>
+[[nodiscard]] str sprint(const char* format_str, Args&&... args) {
+    Array<char> builder = {};
+    array_reserve(&builder, strlen(format_str) + (sizeof...(args) * 8)); // Initial guess
+
+    print_detail::print_impl_recursive(&builder, (char*)format_str, std::forward<Args>(args)...);
+
+    return (str){builder.data, builder.length};
+}
+
+template<typename... Args>
+void fprint(FILE *stream, const char* format_str, Args&&... args) {
+    str string = sprint(format_str, std::forward<Args>(args)...);
+    fwrite(string.data, sizeof(*string.data), string.length, stream);
+    str_free(string);
+}
+
+template<typename... Args>
+void print(const char* format_str, Args&&... args) {
+    fprint(stdout, format_str, std::forward<Args>(args)...);
+}
+
+inline void builder_add(Array<char> *array, str string) {
+    array_add_range(array, string.data, string.length);
+}
+
+namespace print_detail {
+
+// The "extension point" for user-defined types. ADL will find this.
+// The primary template is intentionally left undefined.
+template<typename T>
+void to_str(Array<char>* builder, const T& val);
+
+
+template<typename T>
+void h_format_arg(Array<char>* builder, const T& value) {
+    using DecayedT = std::decay_t<T>;
+
+    if constexpr (std::is_same_v<DecayedT, char*>) {
+        if (value) {
+            builder_add(builder, str_cstr_view(value));
+        } else {
+            builder_add(builder, str("(null)"));
+        }
+    }
+    else if constexpr (std::is_same_v<DecayedT, str>) {
+        builder_add(builder, value);
+    }
+    else if constexpr (std::is_same_v<DecayedT, bool>) {
+        if (value) {
+            builder_add(builder, str("true"));
+        } else {
+            builder_add(builder, str("false"));
+        }
+    }
+    else if constexpr (std::is_integral_v<DecayedT> || std::is_floating_point_v<DecayedT>) {
+        char *buffer = builder->data + builder->length;
+        array_reserve_to_add(builder,                   32       );
+        auto [ptr, ec] = std::to_chars(buffer, buffer + 32, value);
+        assert((ec == std::errc()) && "Internal formtting error");
+        builder->length += ptr - buffer;
+    }
+    else {
+        to_str(builder, value);
+    }
+}
+
+
+// Base case for the recursion: no more arguments to process.
+// Just append the rest of the format string.
+void print_impl_recursive(Array<char>* builder, const char* fmt) {
+    builder_add(builder, str_cstr_view((char*)fmt));
+}
+
+// Recursive step: process one argument and recurse on the rest.
+template<typename T, typename... Rest>
+void print_impl_recursive(Array<char>* builder, const char* fmt, T&& arg, Rest&&... rest) {
+    const char* p = strchr(fmt, '%');
+    if (!p) {
+        // No more specifiers, append the whole rest of the string and we're done.
+        builder_add(builder, str_cstr_view((char*)fmt));
+        return;
+    }
+
+    builder_add(builder, (str){(char*)fmt, (u64)(p - fmt)});
+
+    h_format_arg(builder, std::forward<T>(arg));
+
+    print_impl_recursive(builder, p + 1, std::forward<Rest>(rest)...);
+}
+
+} // namespace print_detail
+
+#endif // PRINT_HPP
+
+// SEE: https://github.com/kraj/musl/blob/kraj/master/src/stdio/vfprintf.c
