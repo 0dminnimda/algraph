75332121b9d5a8e3d40a6279a426e8a94b6c750d
0dminnimda <0dminnimda@gmail.com>
2025-07-09 01:59:33 +0300

    Simplify and improve str_to_int, remove checked_int

--- source/main.cpp
+++ source/main.cpp
@@ -3,78 +3,37 @@
 #include "types.h"
 
 #include <stdio.h>
-#define STR_IMPLEMENT
+
 #include "array.hpp"
 #include "print.hpp"
 
+#define STR_IMPLEMENT
+#include "str.hpp"
+
+#include "str_to_int.hpp"
+
+
 int main() {
-    Array<int> my_ints = {};
+    str a = str("alex");
+    str b = str("255");
+    str c = str("256");
+    str d = str("256");
+    str e = str("-10");
+    str f = str("-129");
 
-    print("adding elements...\n");
-    array_add(&my_ints, 10);
-    array_add(&my_ints, 20);
-    array_add(&my_ints, 30);
+    u8 a_i, b_i, c_i; s16 d_i; s8 e_i, f_i;
+    auto a_r = str_to_int(a, &a_i);
+    auto b_r = str_to_int(b, &b_i);
+    auto c_r = str_to_int(c, &c_i);
+    auto d_r = str_to_int(d, &d_i);
+    auto e_r = str_to_int(e, &e_i);
+    auto f_r = str_to_int(f, &f_i);
 
-    print("Array has % elements, capacity for %\n", my_ints.length, my_ints.capacity);
-    print("Element at index 1: %\n", my_ints[1]);
-
-    int new_data[] = {4, 5, 6, 7, 8, 9, 10};
-    array_add_range(&my_ints, new_data, 7);
-    print("Array has % elements, capacity for %\n", my_ints.length, my_ints.capacity);
-
-    print("Iterating with range-based for loop:\n");
-    for (int val : my_ints) {
-        printf("  - %d\n", val);
-    }
-
-    int popped_value = array_pop(&my_ints);
-    print("Popped value: %\n", popped_value);
-    print("Array now has % elements %\n", my_ints.length, 1);
-
-    array_free(&my_ints);
-    print("Array freed. Length is now %-%\n", my_ints.length, str("hello"));
-
-    print("\nCreating an array with a macro:\n");
-    Array<float> my_floats = array_of(1.1f, 2.2f, 3.3456f, 4.4f);
-
-    for (float f : my_floats) {
-        print("  - %\n", f);
-    }
-
-    array_free(&my_floats);
-
+    print("% -> % (%)\n", a, a_i, (u8)a_r);
+    print("% -> % (%)\n", b, b_i, (u8)b_r);
+    print("% -> % (%)\n", c, c_i, (u8)c_r);
+    print("% -> % (%)\n", d, d_i, (u8)d_r);
+    print("% -> % (%)\n", e, e_i, (u8)e_r);
+    print("% -> % (%)\n", f, f_i, (u8)f_r);
     return 0;
 }
-
-/*#define STR_IMPLEMENT*/
-/*#include "str.hpp"*/
-/**/
-/*#include "str_to_int.hpp"*/
-/**/
-/*#include "hprint.hpp"*/
-/*using namespace hp;*/
-/**/
-/*int main() {*/
-/*    str a = str("alex");*/
-/*    str b = str("255");*/
-/*    str c = str("256");*/
-/*    str d = str("256");*/
-/*    str e = str("-10");*/
-/*    str f = str("-127");*/
-/**/
-/*    u8 a_i, b_i, c_i; s16 d_i; s8 e_i, f_i;*/
-/*    auto a_r = str_to_int(a, &a_i);*/
-/*    auto b_r = str_to_int(b, &b_i);*/
-/*    auto c_r = str_to_int(c, &c_i);*/
-/*    / *auto d_r = str_to_int(d, &d_i);* /*/
-/*    / *auto e_r = str_to_int(e, &e_i);* /*/
-/*    / *auto f_r = str_to_int(f, &f_i);* /*/
-/**/
-/*    print("% -> % (%)\n", a, a_i, (u8)a_r);*/
-/*    print("% -> % (%)\n", b, b_i, (u8)b_r);*/
-/*    print("% -> % (%)\n", c, c_i, (u8)c_r);*/
-/*    / *print("% -> % (%)\n", d, d_i, (u8)d_r);* /*/
-/*    / *print("% -> % (%)\n", e, e_i, (u8)e_r);* /*/
-/*    / *print("% -> % (%)\n", f, f_i, (u8)f_r);* /*/
-/*    return 0;*/
-/*}*/
--- source/str_to_int.hpp
+++ source/str_to_int.hpp
@@ -3,40 +3,7 @@
 
 #include <type_traits>
 #include <limits>
-#include "checked_int.h"
-
-template <typename T>
-constexpr size_t int_bit_width() {
-    static_assert(std::is_integral<T>::value, "T must be an integer type");
-    return std::numeric_limits<T>::digits + (std::numeric_limits<T>::is_signed ? 1 : 0);
-}
-
-template <typename T>
-constexpr size_t int_decimal_digits() {
-    static_assert(std::is_integral<T>::value, "T must be an integer type");
-    int digits = 1;
-    T n = std::numeric_limits<T>::max();
-    while (n >= 10) {
-        n /= 10;
-        digits++;
-    }
-    return digits;
-}
-
-template <typename T>
-constexpr u8 int_most_significant_digit() {
-    static_assert(std::is_integral<T>::value, "T must be an integer type");
-    T n = std::numeric_limits<T>::max();
-    while (n >= 10) {
-        n /= 10;
-    }
-    return n;
-}
-
-
-static_assert(int_bit_width<uint16_t>() == 16);
-static_assert(int_decimal_digits<uint8_t>() == 3);
-static_assert(int_most_significant_digit<int16_t>() == 3);
+#include <cstdint>
 
 enum S2I_Result {
     S2I_OK,
@@ -49,9 +16,6 @@ enum S2I_Result {
 
  * TODO: Allow to pass in a target base.
 
- * TODO: Allow to have leading + or -
- * (no spaces between them and number allowed).
-
  * TODO: Allow single underscores in the number
  * (except the first, last and characteres right after 0b,0o,0x).
  * Not allowed: _234, 456_, 0x_F, 0xF_
@@ -63,16 +27,8 @@ enum S2I_Result {
  * https://github.com/python/cpython/blob/6557af669899f18f8d123f8e1b6c3380d502c519/Objects/longobject.c#L2518
  */
 
-/* The basic idea is that given a spefic type, you know it's maximum/minimum.
- * If you leave out the most significant digit (MSD) (make number out of one less digits then maximum)
- * then you can guarantee, there were no overflows while you are constructing it.
- * The only possible overflow is when you add MSD to the rest.
-
- * For example, for u8 all numbers from 0 to 99 are guranteed to fit,
- * but if you start parting 299, thus adding 200 and 99,
- * then you will need to check for overflow,
- * which ofter should be a single instruction and a jump.
- */
+template <typename T>
+S2I_Result str_to_int_and_consume(str *string, T *value);
 
 template <typename T>
 S2I_Result str_to_int(str string, T *value) {
@@ -82,61 +38,66 @@ S2I_Result str_to_int(str string, T *value) {
 
 template <typename T>
 S2I_Result str_to_int_and_consume(str *string, T *value) {
-    static_assert(std::is_signed<T>::value, "parsing of unsigned ints is not implemented");
+    static_assert(std::is_integral<T>::value, "T must be an integer type");
 
-    if (string->length == 0) return S2I_NOT_FOUND;
-    char c = string->data[0];
+    str temp_str = *string;
 
-    u8 msd;
-    if ('0' <= c && c <= '9') {
-        msd = c - '0';
-    } else {
+    if (temp_str.length == 0) {
         return S2I_NOT_FOUND;
     }
 
-    T msd_multiplier = 1;
-    T rest = 0;
-    constexpr u8 max_digits = int_decimal_digits<T>();
-    u8 count = 1;
-    for (; count < max_digits && count < string->length; ++count) {
-        c = string->data[count];
-        if ('0' <= c && c <= '9') {
-            msd_multiplier *= 10;
-            rest = rest * 10 + (c - '0');
-        } else {
+    bool is_negative = false;
+    size_t consumed_prefix = 0;
+
+    if (std::is_signed<T>::value) {
+        if (temp_str.data[0] == '-') {
+            is_negative = true;
+            consumed_prefix = 1;
+        } else if (temp_str.data[0] == '+') {
+            consumed_prefix = 1;
+        }
+    }
+
+    // String with only a sign, or an empty string for unsigned, is not a valid number.
+    if (temp_str.length <= consumed_prefix) {
+        return S2I_NOT_FOUND;
+    }
+
+    bool found_digit = false;
+    T result = 0;
+
+    size_t i = consumed_prefix;
+    for (; i < temp_str.length; ++i) {
+        char c = temp_str.data[i];
+        if (c < '0' || c > '9') {
             break;
         }
-    }
 
-    if (count != max_digits) {
-        *value = msd * msd_multiplier + rest;
-        string->data += count;
-        string->length -= count;
-        return S2I_OK;
-    }
+        found_digit = true;
+        uint8_t digit = c - '0';
 
-    if (count < string->length) {
-        c = string->data[count];
-        if ('0' <= c && c <= '9') {
-            /* Got more than max digits, definitely not in range. */
-            return S2I_OUT_OF_RANGE;
+        if (is_negative) {
+            constexpr T min_val = std::numeric_limits<T>::min();
+            if (result < min_val / 10 || (result == min_val / 10 && static_cast<T>(-digit) < min_val % 10)) {
+                return S2I_OUT_OF_RANGE;
+            }
+            result = result * 10 - digit;
+        } else {
+            constexpr T max_val = std::numeric_limits<T>::max();
+            if (result > max_val / 10 || (result == max_val / 10 && static_cast<T>(digit) > max_val % 10)) {
+                return S2I_OUT_OF_RANGE;
+            }
+            result = result * 10 + digit;
         }
     }
 
-    constexpr u8 max_value_for_leading_digit
-        = int_most_significant_digit<T>();
-
-    if (msd > max_value_for_leading_digit) {
-        /* Most significant digit value exceeded it's limit */
-        return S2I_OUT_OF_RANGE;
+    if (!found_digit) {
+        return S2I_NOT_FOUND;
     }
-    msd_multiplier *= msd;
 
-    if (ckd_add(value, msd_multiplier, rest)) {
-        return S2I_OUT_OF_RANGE;
-    }
-    string->data += count;
-    string->length -= count;
+    *value = result;
+    string->data += i;
+    string->length -= i;
+
     return S2I_OK;
 }
-
