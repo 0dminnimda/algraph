2c79d8515402c326958387558ad8aa0f8287fc80
0dminnimda <0dminnimda@gmail.com>
2025-07-09 01:06:42 +0300

    nob: add debug

--- nob.c
+++ nob.c
@@ -5,13 +5,16 @@
 #define NO_COMMAND NULL
 
 #define COMPILER "clang++"
-#define COMMON_FLAGS "-O1", "-Wno-unused-const-variable", "-Wno-writable-strings", "-Wno-vla-cxx-extension"
-#define WARNING_FLAGS "-Wall", "-Wextra"
+#define COMMON_FLAGS "-O1"
+#define WARNING_FLAGS "-Wall", "-Wextra", \
+    "-Wno-unused-const-variable", "-Wno-writable-strings", \
+    "-Wno-vla-cxx-extension"
+#define DEBUG_FLAGS "-ggdb", "-O0"
 
 #define SOURCE "source/main.cpp"
 #define EXE "algraph.exe"
 
-bool compile() {
+bool compile(bool in_debug) {
     Cmd cmd = {0};
 
     cmd_append(&cmd, COMPILER);
@@ -19,16 +22,26 @@ bool compile() {
     nob_cc_output(&cmd, EXE);
     cmd_append(&cmd, SOURCE);
 
-    cmd_append(&cmd, WARNING_FLAGS);
     cmd_append(&cmd, COMMON_FLAGS);
+    cmd_append(&cmd, WARNING_FLAGS);
+    if (in_debug) {
+        cmd_append(&cmd, DEBUG_FLAGS);
+    }
 
     return cmd_run_sync(cmd);
 }
 
-bool compile_and_run() {
-    if (!compile()) return 1;
+bool compile_and_run(bool in_debug) {
+    if (!compile(in_debug)) return 1;
 
     Cmd cmd = {0};
+    if (in_debug) {
+        cmd_append(&cmd, "gdb");
+        cmd_append(&cmd, "-ex", "b _assert");
+        cmd_append(&cmd, "-ex", "handle SIGABRT stop nopass");
+        cmd_append(&cmd, "-ex", "handle SIGQUIT stop nopass");
+        cmd_append(&cmd, "-ex", "run", "--args");
+    }
     cmd_append(&cmd, "./"EXE);
     return cmd_run_sync_and_reset(&cmd);
 }
@@ -51,11 +64,11 @@ int main(int argc, char **argv) {
     char *target = get_arg(argv, argc);
 
     if (target == NO_COMMAND) {
-        if (!compile()) return 1;
+        if (!compile(/*in_debug*/false)) return 1;
     } else if (strcmp(target, "run") == 0) {
-        if (!compile_and_run()) return 1;
+        if (!compile_and_run(/*in_debug*/false)) return 1;
     } else if (strcmp(target, "debug") == 0) {
-        if (!compile_and_run()) return 1;
+        if (!compile_and_run(/*in_debug*/true)) return 1;
     } else {
         return help(program);
     }
